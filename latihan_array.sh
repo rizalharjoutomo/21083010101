92e23378c945768d3bf8e2b2ae54ddec12aada75
rizalharjoutomo <113302281+rizalharjoutomo@users.noreply.github.com>
2022-10-16 20:33:14 +0700

    Add files via upload

---
+++ latihan_array.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+printf "Banyak Semester yang sudah diikuti?"
+read semester
+
+declare -a IPSMahasiswa
+
+i=0
+let banyak=$semester-1
+
+while [ $i -le $banyak ]
+do
+	let angka=$i+1
+	printf "nilai Semester %.1i:" $angka;
+	read nilaisemester;
+	IPSMahasiswa[$i]=$nilaisemester;
+	let jumlah=jumlah+$nilaisemester;
+	let i=$i+1;
+done
+
+let IPK=$jumlah/$semester
+echo "nilai per semester" ${IPSMahasiswa[@]}
+echo "nilai IPS:" $jumlah "/" $semester
+echo "nilai IPK:" $IPK
